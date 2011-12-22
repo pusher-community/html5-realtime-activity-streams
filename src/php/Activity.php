4c666d761e1cc9f8cf098922bd1937815f929b18
Phil Leggetter <phil@leggetter.co.uk>
2011-12-22 22:41:22 +0000

    first commit. Needs refactoring and simplified for the tutorial

---
+++ src/php/Activity.php
@@ -0,0 +1,86 @@
+<?php
+class Activity {
+  
+  private $display_name = 'Somebody';
+  private $image = 'http://www.gravatar.com/avatar/00000000000000000000000000000000?d=monsterid&s=48';
+  private $action_text = null;
+  private $date = null;
+  private $id;
+  
+  public function __construct($activity_type, $action_text, $email = null) {
+    date_default_timezone_set('UTC');
+    $this->id = uniqid();
+    $this->date = date('Y-m-d H:i:s');
+    
+    $this->action_text = $action_text;
+    
+    if( is_null($email) == false ) {
+      $this->image = $this->get_gravatar($email);
+      $profile = $this->get_gravatar_profile($email);
+      if( isset($profile['displayName']) ) {
+        $this->display_name = $profile['displayName'];
+      }
+    }
+  }
+  
+  public function getMessage() {
+    $activity = array(
+      'id' => $this->id,
+      'body' => $this->action_text,
+      'published' => $this->date,
+      'actor' => array(
+        'displayName' => $this->display_name,
+        'objectType' => 'person',
+        'image' => array(
+          'url' => $this->image,
+          'width' => 48,
+          'height' => 48
+        )
+      )
+    );
+    return $activity;
+  }
+  
+  // from: http://en.gravatar.com/site/implement/images/php/
+  /**
+   * Get either a Gravatar URL or complete image tag for a specified email address.
+   *
+   * @param string $email The email address
+   * @param string $s Size in pixels, defaults to 80px [ 1 - 512 ]
+   * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
+   * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
+   * @param boole $img True to return a complete IMG tag False for just the URL
+   * @param array $atts Optional, additional key/value attributes to include in the IMG tag
+   * @return String containing either just a URL or a complete image tag
+   * @source http://gravatar.com/site/implement/images/php/
+   */
+  private function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
+  	$url = 'http://www.gravatar.com/avatar/';
+  	$url .= $this->get_email_hash( $email );
+  	$url .= "?s=$s&d=$d&r=$r";
+  	if ( $img ) {
+  		$url = '<img src="' . $url . '"';
+  		foreach ( $atts as $key => $val )
+  			$url .= ' ' . $key . '="' . $val . '"';
+  		$url .= ' />';
+  	}
+  	return $url;
+  }
+  
+  private function get_gravatar_profile($email) {
+    $profile = null;
+    $hash = $this->get_email_hash($email);
+    $str = file_get_contents( 'http://www.gravatar.com/' . $hash . '.php' );
+    $unserialized = unserialize( $str );
+    if ( is_array( $unserialized ) && isset( $unserialized['entry'] ) ) {
+      $profile = $unserialized['entry'][0];
+    }
+    return $profile;
+  }
+  
+  private function get_email_hash($email) {
+    return md5( strtolower( trim( $email ) ) );
+  }
+  
+}
+?>
---
+++ src/php/trigger_activity.php
@@ -0,0 +1,44 @@
+<?php
+require_once('../lib/squeeks-Pusher-PHP/lib/Pusher.php');
+require_once('Activity.php');
+require_once('config.php');
+
+$activity_type = $_GET['activity_type'];
+$activity_data = null;
+$email = null;
+
+if( isset($_GET['activity_data']) ){
+  $activity_data = $_GET['activity_data'];
+}
+if( isset($_GET['email']) ){
+  $email = $_GET['email'];
+}
+
+$action_text = getActionText($activity_type, $activity_data);
+
+$activity = new Activity($activity_type, $action_text, $email);
+
+$pusher = new Pusher(APP_KEY, APP_SECRET, APP_ID);
+$pusher->trigger('site-activity', $activity_type, $activity->getMessage());
+
+/****************************************************************************************/
+
+function getActionText($activity_type, $activity_data) {
+  $action_text = 'just did something unrecognisable.';
+  switch($activity_type) {
+    case 'page-load':
+      $action_text = 'just navigated to the Activity Streams example page.';
+      break;
+    case 'test-event':
+      $action_text = 'just clicked the test button.';
+      break;
+    case 'scroll':
+      $action_text = 'just scrolled to the '. $activity_data['position'] . ' of the page';
+      break;
+    case 'like':
+      $action_text = 'just liked: "'. $activity_data['text'] . '"';
+      break;
+  }
+  return $action_text;
+}
+?>
